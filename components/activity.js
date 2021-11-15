d9b0e99311a0676ebadac4f1953cb3db7a471e3a
unknown <chahine.benlahcen@gmail.com>
2021-11-15 16:36:28 +0100

    fixx

--- components/activity.js
+++ components/activity.js
@@ -7,20 +7,18 @@ export default function activity() {
         userId: "366670764939804672",
         socket: true,
     });
-    const [name, setName] = useState('')
+    const [name, setName] = useState(false)
     console.log(status)
     console.log(status?.activities[0])
     useEffect(() => {
         if (status) {
-            if (status.activities) {
+            if (status.assets) {
                 const name = status?.activities[0]?.name?.replace("Code", "Visual Studio Code");
                 return setName(name)
             } else {
-                setName('')
+                setName(false)
             }
-        } else {
-            setName('')
-        }
+        } 
 
     }, [status])
 
@@ -38,7 +36,7 @@ export default function activity() {
     return (
 
         <div className="w-80 bg-gray-800 bg-opacity-20 text-white rounded p-4 m-8 dark:text-gray-300">
-            {name !== '' && status?.activities[0].assets.small_text === "Visual Studio Code" ?
+            {name && status?.activities[0].assets.small_text === "Visual Studio Code" ?
                 <div className="flex items-center space-x-4">
                     <img src={`https://cdn.discordapp.com/app-assets/${status?.activities[0]?.application_id}/${status?.activities[0].assets.large_image}.png`} className="flex-shrink-0 w-16 h-16 rounded-xl" />
                     <div className="flex flex-col space-y-1 text-sm">
