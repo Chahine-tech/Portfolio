c80fdd82903d8d5d93eca2ede933ab8fc9cb44b9
unknown <chahine.benlahcen@gmail.com>
2021-11-15 15:49:14 +0100

    debug vercel again

--- components/activity.js
+++ components/activity.js
@@ -7,22 +7,31 @@ export default function activity() {
         userId: "366670764939804672",
         socket: true,
     });
+    const [name, setName] = useState('')
+    console.log(status)
     console.log(status?.activities[0])
-    const name = status?.activities[0]?.name?.replace("Code", "Visual Studio Code");
-
-/* const setActivity = () => {
+    useEffect(() => {
         if (status) {
-            return status;
+            const name = status?.activities[0]?.name?.replace("Code", "Visual Studio Code");
+            setName(name)
         }
-        else {
-            return 
+    }, [status])
+
+
+
+    /* const setActivity = () => {
+            if (status) {
+                return status;
+            }
+            else {
+                return 
+            }
         }
-    }
-*/
+    */
     return (
 
         <div className="w-80 bg-gray-800 bg-opacity-20 text-white rounded p-4 m-8 dark:text-gray-300">
-            {status?.activities[0].assets.small_text==="Visual Studio Code"?
+            { status !== undefined || status?.activities[0].assets.small_text === "Visual Studio Code" ?
                 <div className="flex items-center space-x-4">
                     <img src={`https://cdn.discordapp.com/app-assets/${status?.activities[0]?.application_id}/${status?.activities[0].assets.large_image}.png`} className="flex-shrink-0 w-16 h-16 rounded-xl" />
                     <div className="flex flex-col space-y-1 text-sm">
