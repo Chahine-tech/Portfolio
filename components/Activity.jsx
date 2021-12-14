f75421a526a81d47d24ed5f4289c605fa13e2072
unknown <chahine.benlahcen@gmail.com>
2021-12-14 17:12:02 +0100

    update activity

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -47,7 +47,13 @@ export default function activity() {
                         <h1> {status?.activities[0].state}</h1>
                     </div>
                 </div>
-                : <h1 className="text-sm">I'm not currently doing anything</h1>}
+                : <div className="">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <h1 className="text-sm">I'm not currently doing anything</h1>
+                </div>
+            }
         </div>
 
     )
