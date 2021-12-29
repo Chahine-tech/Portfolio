82652e412f33c84567d4804c92334e7fdc64e623
unknown <chahine.benlahcen@gmail.com>
2021-12-29 14:29:14 +0100

    update prod

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -40,7 +40,7 @@ export default function activity() {
         <div className="w-80 bg-gray-800 bg-opacity-20 text-white rounded p-4 m-8 dark:text-gray-300">
             {name !== '' && status?.activities[0].assets.small_text === "Visual Studio Code" ?
                 <div className="flex items-center space-x-4">
-                    <img src={`https://cdn.discordapp.com/app-assets/${status?.activities[0]?.application_id}/${status?.activities[0].assets.large_image}.png`} className="flex-shrink-0 w-16 h-16 rounded-xl" />
+                    <img src={`https://cdn.discordapp.com/app-assets/${status?.activities[0]?.application_id}/${status?.activities[0].assets.large_image}.png`} className="flex-shrink-0 w-16 h-16 rounded-xl" alt="Visual Studio Code" />
                     <div className="flex flex-col space-y-1 text-sm">
                         <h1> {name} </h1>
                         <h1> {status?.activities[0].details}</h1>
--- components/ContainerBlock.jsx
+++ components/ContainerBlock.jsx
@@ -20,14 +20,6 @@ export default function ContainerBlock({ children, ...customMeta }) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://yourwebsite.com${router.asPath}`}
-        />
-        <link
-          rel="canonical"
-          href={`https://yourwebsite.com${router.asPath}`}
-        />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Chahine" />
         <meta property="og:description" content={meta.description} />
