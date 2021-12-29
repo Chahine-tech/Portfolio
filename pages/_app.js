c2c4c195930e70ab55e0c314a8ab15ac4f025e20
unknown <chahine.benlahcen@gmail.com>
2021-12-29 19:30:30 +0100

    add progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
