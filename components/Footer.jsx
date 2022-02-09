62d2fb3ee6e56c339a91e856afbf65fc6ba646b3
unknown <chahine.benlahcen@gmail.com>
2022-02-09 15:01:23 +0100

    edit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,6 @@ export default function Footer() {
         <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
           <div>
-            {/* Support me by keeping this in the footer, please. :) */}
             <p>
               Developed by
               <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
